59f3c6a9d2890cd443335bcde24e99aaa7d18254
Deibson Fagner <deibson.fagner@gmail.com>
2024-10-10 08:30:07 -0300

    Update init_projet_python.sh

--- init_projet_python.sh
+++ init_projet_python.sh
@@ -66,31 +66,87 @@ if ! command -v pip3 >/dev/null; then
     if [[ $INSTALAR_PIP3 =~ ^[Ss]$ ]]; then
         echo "Instalando o pip3..."
         sudo apt-get install python3-pip -y
+        python3 -m pip install --upgrade pip
     else
         echo "O pip3 não será instalado."
     fi
 fi
 
-# Cria o diretório para armazenar os arquivos Python do projeto
-mkdir src
-
-# Cria o diretório para documentação (opcional).
+# Criar o diretório para documentação (opcional).
 mkdir docs
 
-# Cria o arquivo principal (geralmente main.py)
-touch main.py
+# Criar o arquivo de MANIFEST (opcional)
+touch MANIFEST.in
 
-# Cria o arquivo de requisitos (opcional)
+# Criar o arquivo de LICENSE (opcional)
+touch LICENSE.txt
+
+# Criar o arquivo de requisitos (opcional)
 touch requirements.txt
 
-# Cria o arquivo README.md (opcional)
+# Criar o arquivo de requisitos de test(opcional)
+touch test-requirements.txt
+
+# Criar o arquivo README.md (opcional)
 touch README.md
 
-# Cria o arquivo gitignore (opcional)
+# Criar o arquivo gitignore (opcional)
 touch .gitignore
 
-# Gera conteúdo do README.md
-CONTEUDO_README=:'
+# Criar o arquivo setup (opcional)
+touch setup.py
+
+# Criar o arquivo pypirc (opcional)
+touch .pypirc
+
+# Gerar conteúdo do MANIFEST.in
+CONTEUDO_MANIFEST='include LICENSE
+include README.md
+include requirements.txt
+'
+
+# Grava o conteúdo no requirements.txt
+echo "$CONTEUDO_MANIFEST" > MANIFEST.in
+
+# Gerar conteúdo do LICENSE.txt
+CONTEUDO_LICENSE='MIT License
+
+Copyright (c) 2023 ArjanCodes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'
+
+# Grava o conteúdo no LICENSE.txt
+echo "$CONTEUDO_LICENSE" > LICENSE.txt
+
+# Gerar conteúdo do requirements.txt
+CONTEUDO_REQUIREMENTS='twine
+setuptools
+wheel
+setuptools-declarative-requirements
+'
+
+# Grava o conteúdo no requirements.txt
+echo "$CONTEUDO_REQUIREMENTS" > requirements.txt
+
+# Gerar conteúdo do README.md
+CONTEUDO_README='
 
 ## NOME_PROJETO
 
@@ -126,12 +182,212 @@ Para desativar o ambiente virtual e voltar ao terminal principal, utilize o coma
 # Grava o conteúdo no README.md
 echo "$CONTEUDO_README" > README.md
 
-# Grava o conteúdo no gitignore
-echo ".venv" > .gitignore
+# Gerar conteúdo do .gitignore
+CONTEUDO_GITIGNORE='# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
 
-# Grava o conteúdo no main.py
-echo "if __name__ == "__main__":
-    print("Hello, World!")" > main.py
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+.pypirc
+MANIFEST
+
+# PyInstaller
+#  Usually these files are written by a python script from a template
+#  before PyInstaller builds the exe, so as to inject date/other infos into it.
+*.manifest
+*.spec
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+cover/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+.pybuilder/
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# IPython
+profile_default/
+ipython_config.py
+
+# pyenv
+#   For a library or package, you might want to ignore these files since the code is
+#   intended to run in multiple environments; otherwise, check them in:
+# .python-version
+
+# pipenv
+#   According to pypa/pipenv#598, it is recommended to include Pipfile.lock in version control.
+#   However, in case of collaboration, if having platform-specific dependencies or dependencies
+#   having no cross-platform support, pipenv may install dependencies that dont work, or not
+#   install all needed dependencies.
+#Pipfile.lock
+
+# PEP 582; used by e.g. github.com/David-OConnor/pyflow
+__pypackages__/
+
+# Celery stuff
+celerybeat-schedule
+celerybeat.pid
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+# Pyre type checker
+.pyre/
+
+# pytype static type analyzer
+.pytype/
+
+# Cython debug symbols
+cython_debug/
+'
+
+# Grava o conteúdo no gitignore
+echo "$CONTEUDO_GITIGNORE" > .gitignore
+
+# Gerar conteúdo do setup.py
+CONTEUDO_SETUP='from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="package_name",
+    version="0.0.1",
+    author="my_name",
+    author_email="my_email",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_repository_project_link",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "dev": ["pytest", "twine"],
+    },
+    license="MIT",
+    zip_safe=False,
+    test_suite="unittest",
+)
+'
+
+# Grava o conteúdo no setup.py
+echo "$CONTEUDO_SETUP" > setup.py
+
+# Gerar conteúdo do .pypirc
+CONTEUDO_PYPIRC='[distutils]
+index-servers=
+    pypi
+    pypitest
+
+[pypitest]
+repository = https://testpypi.python.org/pypi
+username = user_name
+
+[pypi]
+repository = https://pypi.python.org/pypi
+username = user_name
+'
+
+# Grava o conteúdo no .pypirc
+echo "$CONTEUDO_PYPIRC" > .pypirc
+
+# Instalar dependencias python basicas
+pip install -r requirements.txt
 
 # Verifica se o nome e email do Git estão configurados
 NOME_GIT=$(git config user.name)
@@ -163,14 +419,49 @@ if [ -z "$NOME_GIT" ] || [ -z "$EMAIL_GIT" ]; then
     fi
 fi
 
-# Inicializa o repositório Git (opcional)
+
+# Criar o pacote do projeto
+mkdir "$NOME_PROJETO"
+
+# Navegar para o pacote do projeto
+cd "$NOME_PROJETO"
+
+# Criar o arquivo init (geralmente main.py)
+touch __init__.py
+
+# Criar o diretório para armazenar os arquivos Python do projeto
+mkdir src
+
+# Criar o arquivo init 
+touch src/__init__.py
+
+# Criar o arquivo principal (geralmente main.py)
+touch main.py
+
+# Gravar o conteúdo no main.py
+echo 'def print_message():
+    print("Hello, World!")' > main.py
+
+# Gravar o conteúdo no __init__.py
+echo 'from .main import *' > __init__.py
+
+# Navegar para o Diretorio do Projeto
+cd ..
+
+# Criar o diretório para armazenar os arquivos de tests do projeto
+mkdir tests
+
+# Inicializar o repositório Git (opcional)
 git init
 
-# Adiciona os arquivos ao repositório Git (opcional)
-git add main.py requirements.txt README.md .gitignore
+# Adicionar os arquivos ao repositório Git (opcional)
+git add .
 
-# Faz o commit inicial (opcional)
+# Fazer o commit inicial (opcional)
 git commit -m "inital commit"
+
+# Executar Build do Pacote
+python setup.py sdist bdist_wheel
 
 # Exibe mensagem de conclusão
 echo "Projeto inicial Python '$NOME_PROJETO' criado com sucesso!"
